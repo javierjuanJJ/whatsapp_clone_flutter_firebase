08624dc65b956bfd5b702262bda0c8c26ce6115b
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-10-23 16:15:09 +0200

    Commit 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,56 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:whatsapp_clone_flutter_firebase/colors.dart';
-import 'package:whatsapp_clone_flutter_firebase/screens/mobile_layout_screen.dart';
-import 'package:whatsapp_clone_flutter_firebase/screens/web_layout_screen.dart';
-import 'package:whatsapp_clone_flutter_firebase/utils/responsive_layout.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:whatsapp_clone_flutter_firebase/common/utils/colors.dart';
+import 'package:whatsapp_clone_flutter_firebase/common/widgets/error.dart';
+import 'package:whatsapp_clone_flutter_firebase/common/widgets/loader.dart';
+import 'package:whatsapp_clone_flutter_firebase/features/auth/controller/auth_controller.dart';
+import 'package:whatsapp_clone_flutter_firebase/features/landing/screens/landing_screen.dart';
+import 'package:whatsapp_clone_flutter_firebase/firebase_options.dart';
+import 'package:whatsapp_clone_flutter_firebase/router.dart';
+import 'package:whatsapp_clone_flutter_firebase/mobile_layout_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Whatsapp UI',
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: backgroundColor,
+        appBarTheme: const AppBarTheme(
+          color: appBarColor,
+        ),
       ),
-      home: const ResponsiveLayout(
-        mobileScreenLayout: MobileLayoutScreen(),
-        webScreenLayout: WebLayoutScreen(),
-      ),
+      onGenerateRoute: (settings) => generateRoute(settings),
+      home: ref.watch(userDataAuthProvider).when(
+            data: (user) {
+              if (user == null) {
+                return const LandingScreen();
+              }
+              return const MobileLayoutScreen();
+            },
+            error: (err, trace) {
+              return ErrorScreen(
+                error: err.toString(),
+              );
+            },
+            loading: () => const Loader(),
+          ),
     );
   }
 }
